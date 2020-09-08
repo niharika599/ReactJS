295c0153321a54522718fdb8385af92e919c86e9
niharika599 <47943129+niharika599@users.noreply.github.com>
2020-09-08 08:55:37 +0530

    Add files via upload
    JS files

---
+++ js/new.js
@@ -0,0 +1,29 @@
+var details={
+    name:"APSSDC",
+    age:5,
+    courses:["webdesign using react js","python","data analysis"]
+}
+
+//for (var i=0; i<details.courses.length;i++){
+    //document.write(details[i]+"<br>");
+//}
+
+
+/*for (var i in details){
+    document.write(details[i]+"<br>");
+}
+var name="APSSDC ReactJS";
+for(var i of name){
+    document.write(i);
+}*/
+
+var a=25;
+if(a<=21){
+    document.write("success");
+}
+else if(a==25){
+    document.write("Your age is "+a);
+}
+else{
+    document.write("failed")
+}
